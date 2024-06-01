a20eb2e61821dee4f28da8ccda85dc45437e00ef
itkirk6 <itkirk6@gmail.com>
2024-06-01 18:37:46 -0500

    fixed issues with chatgpt prompt

--- decide.py
+++ decide.py
@@ -12,13 +12,19 @@ class Decider:
         #print(descriptions)
         prompt = f"""
 I have a list of command descriptions and their corresponding return values. Here are the descriptions:
-{"\n".join(descriptions)}
+keyword: add, Add item to inventory, ['item name']
+keyword: remove, Remove item from inventory, ['item name']
+keyword: used, Consumed/used some item, ['item name', 'int: amount']
+keyword: shop, Going shopping or needs shopping list, []
+keyword: receipt, Coming back from a shopping trip or has receipt, []
+keyword: out, Ran out of some item, ['item name']
 
 Given the following prompt:
 {input_string}
 
-Give me the index of the description it most matches, and fill in the return value with the data from the prompt it needs.
-This should be in the format: index,returnValue
+Give me the keyword of the description it most matches, and fill in the return value with the data from the prompt it needs.
+This should be in the format: keyword,returnValue
+The item name should not include a number, if the amount is not listed that is extraneous information.
 If the prompt does not seem to match any of the descriptions, print False.
 Do not print or give me anything other than this explicit format I asked for.
 """
@@ -35,19 +41,21 @@ Do not print or give me anything other than this explicit format I asked for.
             print("ChatGPT Error")
     
         msg = completion.choices[0].message.content
+        print(msg)
 
         if msg == "False":
             return False
-        
+        """
         try:
-            index = int(msg.split(",")[0])
+            #index = int(msg.split(",")[0])
 
             argument = ",".join([x.strip() for x in msg.split(",")[1:]]).replace("[", "").replace("]", "").replace('"', "")
             args = argument.split(",")
 
             return args
 
-        except:
-            print("Unable to parse GPT output")
-            return False
+        except Exception as e:
+            print(e)
+            print(msg)
+            return False"""
         
