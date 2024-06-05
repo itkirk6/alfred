92b327502bdc97212404f7f63577f188251a65fe
jam9151 <mcnaryjesse@gmail.com>
2024-06-05 00:48:31 -0500

    finished up

--- dbtest.py
+++
@@ -1,9 +0,0 @@
-import database
-
-db = database.Database()
-print(db.getInventory())
-
-print(db.getInventory())
-(print(db.getList()))
-
-db.goShopping()
--- skit.py
+++
@@ -1,7 +0,0 @@
-import tts
-input()
-tts.textToSpeech("Self Destruct in")
-#tts.textToSpeech("Detonating in")
-tts.textToSpeech("5,, 4,, 3,, 2,, 1")
-# tts.textToSpeech("2")
-# tts.textToSpeech("1")
